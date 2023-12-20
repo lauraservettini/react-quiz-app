4c1b98296c297364f03c22d278978d8e668ea0f7
lauraservettini <lauraservettini87@gmail.com>
2023-12-20 21:01:39 +0100

    add Answers and QuestionTimer components, modify Quiz one

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,21 +1,42 @@
-import {useState} from 'react';
+import {useState, useCallback} from 'react';
 
 import QUESTIONS from '../question';
 import quizCompleteImg from '../assets/quiz-complete.png'
+import QuestionTimer from './QuestionTimer';
+import Answers from './Answers';
 
 export default function Quiz(){
+    const [ answerState, setAnswerState ] = useState('');
     const [ answers, setAnswers] = useState([]);
 
     // deriva l'index relativo alla domanda dalla lunghezza dell'array delle risposte
-    const activeQuestionIndex = answers.length;
+    const activeQuestionIndex = 
+        answerState === '' ? answers.length : answers.length - 1;
     
     const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
     
-    function handleSelectAnswer(selectedAnswer){
+    const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer){
+        setAnswerState('answered');
         setAnswers((prevAnswers) => {
             return [...prevAnswers, selectedAnswer];
         });
-    }
+
+        // evidenzia per 1 sec se la risposta è giusta o sbagliata
+        setTimeout(() => {
+            if(selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]){
+                setAnswerState('correct');
+            }
+            else {
+                setAnswerState('wrong');
+            }
+        setTimeout(() =>{
+            setAnswerState('');
+        }, 2000);
+        }, 1000);
+
+    }, [activeQuestionIndex]);
+
+    const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
     
     if(quizIsComplete){
         return (
@@ -25,24 +46,25 @@ export default function Quiz(){
             </div>
 
         );
-    }
-
-    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-    shuffledAnswers.sort(() => Math.random() - 0.5);
-
+    } 
+    
     return (
         <div id="quiz">
             <div id="question">
+                <QuestionTimer 
+                    key={activeQuestionIndex}
+                    timeout={10000} 
+                    onTimeout={() => handleSkipAnswer(null)} />
                 <h2>
                     {QUESTIONS[activeQuestionIndex].text}
                 </h2>
-                <ul id="answers">
-                    {shuffledAnswers.map((answer) => (
-                        <li key={answer} className="answer">
-                            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
-                        </li>
-                        ))}
-                </ul>
+                <Answers 
+                    key={activeQuestionIndex+1}
+                    answers={QUESTIONS[activeQuestionIndex].answers} 
+                    selectedAnswer={answers[answers.length - 1]} 
+                    answerState={answerState}
+                    onSelect={handleSelectAnswer}
+                    />
             </div>
         </div>
     );
